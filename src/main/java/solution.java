8474ce880aeecf1e23ef57a66e7a990b18cbcffb
gazizkv <valitovgaziz@gmail.com>
2021-07-11 16:17:01 +0500

    averageTimePerMicrotask added to class Task

--- src/main/java/solution.java
+++ src/main/java/solution.java
@@ -1,27 +1,27 @@
 import java.io.*;
-import java.nio.file.Path;
-import java.text.DateFormat;
 import java.util.*;
 
 public class solution {
-    public static Set<Task> setOfTask = new HashSet<>();
-    public static File file = new File("../resources/file.txt");
+    public static ArrayList<Task> arrayList = new ArrayList<>();
+    public static File file = new File("C:\\Users\\valit\\progectManagerTestTask\\src\\main\\resources\\file.txt");
 
     public static class Task {
-        Double tid;
-        Short microtasks;
+        Integer tid;
+        Double microtasks;
         Calendar assinged_ts;
         Calendar closed_ts;
+        Double averageTimePerMicrotask;
 
-        public Task(Double tid, Short microtasks, Calendar assinged_ts, Calendar closed_ts) {
+        public Task(Integer tid, Double microtasks, Calendar assinged_ts, Calendar closed_ts) {
             this.tid = tid;
             this.microtasks = microtasks;
             this.assinged_ts = assinged_ts;
             this.closed_ts = closed_ts;
+            averageTimePerMicrotask = (closed_ts.getTimeInMillis() - assinged_ts.getTimeInMillis())/(microtasks*1000);
         }
     }
 
-    public static Task parseNextTask(String[] nextLine) {
+    public static Task parseNextTaskAndGetNewTask(String[] nextLine) {
         Calendar calendarAssigned_ts = new GregorianCalendar();
         calendarAssigned_ts.set(Calendar.YEAR, Integer.parseInt(nextLine[3].split("-")[0]));
         calendarAssigned_ts.set(Calendar.MONTH, Integer.parseInt(nextLine[3].split("-")[1]));
@@ -30,14 +30,14 @@ public class solution {
         calendarAssigned_ts.set(Calendar.MINUTE, Integer.parseInt(nextLine[4].split(":")[1]));
         calendarAssigned_ts.set(Calendar.SECOND, Integer.parseInt(nextLine[4].split(":")[2]));
         Calendar calendarClosed_ts = new GregorianCalendar();
-        calendarClosed_ts.set(Calendar.YEAR, Integer.parseInt(nextLine[4].split("-")[0]));
-        calendarClosed_ts.set(Calendar.MONTH, Integer.parseInt(nextLine[4].split("-")[1]));
-        calendarClosed_ts.set(Calendar.DATE, Integer.parseInt(nextLine[4].split("-")[2]));
-        calendarClosed_ts.set(Calendar.HOUR, Integer.parseInt(nextLine[5].split(":")[0]));
-        calendarClosed_ts.set(Calendar.MINUTE, Integer.parseInt(nextLine[5].split(":")[1]));
-        calendarClosed_ts.set(Calendar.SECOND, Integer.parseInt(nextLine[5].split(":")[2]));
-        return new Task(Double.parseDouble(nextLine[1]),
-                Short.parseShort(nextLine[2]),
+        calendarClosed_ts.set(Calendar.YEAR, Integer.parseInt(nextLine[5].split("-")[0]));
+        calendarClosed_ts.set(Calendar.MONTH, Integer.parseInt(nextLine[5].split("-")[1]));
+        calendarClosed_ts.set(Calendar.DATE, Integer.parseInt(nextLine[5].split("-")[2]));
+        calendarClosed_ts.set(Calendar.HOUR, Integer.parseInt(nextLine[6].split(":")[0]));
+        calendarClosed_ts.set(Calendar.MINUTE, Integer.parseInt(nextLine[6].split(":")[1]));
+        calendarClosed_ts.set(Calendar.SECOND, Integer.parseInt(nextLine[6].split(":")[2]));
+        return new Task(Integer.parseInt(nextLine[1].substring(0, nextLine[1].length()-2)),
+                Double.parseDouble(nextLine[2]),
                 calendarAssigned_ts,
                 calendarClosed_ts);
     }
@@ -46,12 +46,17 @@ public class solution {
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             String[] inputStringLine;
-            while (true) {
-                inputStringLine = bufferedReader.readLine().split("\\t");
-                if(inputStringLine[0].equals("login")) continue;
-                else if(inputStringLine.length == 0) break;
-                else {
-                    setOfTask.add(parseNextTask(inputStringLine));
+            while (bufferedReader.ready()) {
+                inputStringLine = bufferedReader.readLine().split("[\\t ]");
+                if(!inputStringLine[0].contains("0")) continue;
+                else arrayList.add(parseNextTaskAndGetNewTask(inputStringLine));
+            }
+            if(arrayList.size() > 10 ) {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(arrayList.get(i).averageTimePerMicrotask);
+                    System.out.println(arrayList.get(i).assinged_ts.getTime());
+                    System.out.println(arrayList.get(i).closed_ts.getTime());
+                    System.out.println(arrayList.get(i).tid);
                 }
             }
         } catch (FileNotFoundException e) {
