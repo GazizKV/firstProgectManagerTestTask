4aacb5f7743a9d9ae58e9b2dc0d8607919360251
gazizkv <valitovgaziz@gmail.com>
2021-07-11 10:59:19 +0500

    parseTask and add to arrayLIst

--- src/main/java/solution.java
+++ src/main/java/solution.java
@@ -1,12 +1,13 @@
 import java.io.*;
 import java.nio.file.Path;
+import java.text.DateFormat;
 import java.util.*;
 
 public class solution {
     public static Set<Task> setOfTask = new HashSet<>();
     public static File file = new File("../resources/file.txt");
 
-    public class Task {
+    public static class Task {
         Double tid;
         Short microtasks;
         Calendar assinged_ts;
@@ -20,6 +21,27 @@ public class solution {
         }
     }
 
+    public static Task parseNextTask(String[] nextLine) {
+        Calendar calendarAssigned_ts = new GregorianCalendar();
+        calendarAssigned_ts.set(Calendar.YEAR, Integer.parseInt(nextLine[3].split("-")[0]));
+        calendarAssigned_ts.set(Calendar.MONTH, Integer.parseInt(nextLine[3].split("-")[1]));
+        calendarAssigned_ts.set(Calendar.DATE, Integer.parseInt(nextLine[3].split("-")[2]));
+        calendarAssigned_ts.set(Calendar.HOUR, Integer.parseInt(nextLine[4].split(":")[0]));
+        calendarAssigned_ts.set(Calendar.MINUTE, Integer.parseInt(nextLine[4].split(":")[1]));
+        calendarAssigned_ts.set(Calendar.SECOND, Integer.parseInt(nextLine[4].split(":")[2]));
+        Calendar calendarClosed_ts = new GregorianCalendar();
+        calendarClosed_ts.set(Calendar.YEAR, Integer.parseInt(nextLine[4].split("-")[0]));
+        calendarClosed_ts.set(Calendar.MONTH, Integer.parseInt(nextLine[4].split("-")[1]));
+        calendarClosed_ts.set(Calendar.DATE, Integer.parseInt(nextLine[4].split("-")[2]));
+        calendarClosed_ts.set(Calendar.HOUR, Integer.parseInt(nextLine[5].split(":")[0]));
+        calendarClosed_ts.set(Calendar.MINUTE, Integer.parseInt(nextLine[5].split(":")[1]));
+        calendarClosed_ts.set(Calendar.SECOND, Integer.parseInt(nextLine[5].split(":")[2]));
+        return new Task(Double.parseDouble(nextLine[1]),
+                Short.parseShort(nextLine[2]),
+                calendarAssigned_ts,
+                calendarClosed_ts);
+    }
+
     public static void main(String[] args) {
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
@@ -27,7 +49,10 @@ public class solution {
             while (true) {
                 inputStringLine = bufferedReader.readLine().split("\\t");
                 if(inputStringLine[0].equals("login")) continue;
-
+                else if(inputStringLine.length == 0) break;
+                else {
+                    setOfTask.add(parseNextTask(inputStringLine));
+                }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
